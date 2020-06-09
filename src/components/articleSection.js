0a0cfbc838f0e80a1bcb643b41df38b0d6fedee7
theFl00f <celeste.ellerby@gmail.com>
2020-06-08 21:53:36 -0400

    added articles section

---
+++ src/components/articleSection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import articleImg1 from '../assets/home-image-2.jpg'
+import articleImg2 from '../assets/home-image-3.jpg'
+
+export default function ArticleSection() {
+    return (
+        <section class="article-links">
+        <div class="wrapper">
+            <article class="article1">
+                <div class="article-img-container img1">
+                    <img src={articleImg1} alt="a dog with his head hanging out an orange vintage vehicle"/>
+                </div>
+                <div class="article-text-container">
+                    <h3>sophie's favourite things</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi quaerat, dolor ut exercitationem libero id dignissimos quia facere unde. Hic aliquam animi esse maxime temporibus nemo numquam.</p>
+                    <button>
+                        <a href="#">
+                            read more
+                        </a>
+                    </button>
+                </div>
+            </article>
+            <article class="article2">
+                <div class="article-text-container">
+                    <h3>where she's travelled</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae veritatis natus sit laboriosam quam sequi commodi? Dolore est ipsam unde, recusandae velit, corrupti, et laborum quasi ut rerum provident enim!</p>
+                    <button>
+                        <a href="#">
+                            read more
+                        </a>
+                    </button>
+                </div>
+                <div class="article-img-container img2">
+                    <img src={articleImg2} alt="a sandy beach with blue chairs and orange umbrellas."/>
+                </div>
+            </article>
+        </div>
+    </section>
+    )
+}
--- src/components/indexMain.js
+++ src/components/indexMain.js
@@ -1,42 +1,13 @@
 import React from 'react';
 import AboutSection from './aboutSection';
+import ArticleSection from './articleSection';
 
 export default function IndexMain() {
     return (
         <main>
             <AboutSection />
-        {/* <section class="article-links">
-            <div class="wrapper">
-                <article class="article1">
-                    <div class="article-img-container img1">
-                        <img src="./assets/home-image-2.jpg" alt="a dog with his head hanging out an orange vintage vehicle">
-                    </div>
-                    <div class="article-text-container">
-                        <h3>sophie's favourite things</h3>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi quaerat, dolor ut exercitationem libero id dignissimos quia facere unde. Hic aliquam animi esse maxime temporibus nemo numquam.</p>
-                        <button>
-                            <a href="#">
-                                read more
-                            </a>
-                        </button>
-                    </div>
-                </article>
-                <article class="article2">
-                    <div class="article-text-container">
-                        <h3>where she's travelled</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae veritatis natus sit laboriosam quam sequi commodi? Dolore est ipsam unde, recusandae velit, corrupti, et laborum quasi ut rerum provident enim!</p>
-                        <button>
-                            <a href="#">
-                                read more
-                            </a>
-                        </button>
-                    </div>
-                    <div class="article-img-container img2">
-                        <img src="./assets/home-image-3.jpg" alt="a sandy beach with blue chairs and orange umbrellas.">
-                    </div>
-                </article>
-            </div>
-        </section> */}
+
+            <ArticleSection />
         <section class="info">
             <div class="wrapper">
                 <div class="info-container">
